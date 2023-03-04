a9c1b36d6f16c010bb8c8b0f3869c740e17f3313
Husain4753 <Husaintinwala79@gmail.com>
2023-03-04 20:30:34 +0530

    1st Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello React</h1>
+    <Button variant="contained" color='success' >contained</Button>
+    <Button variant="outlined">contained</Button><br /><br />
+    <Button variant="contained" color='warning' startIcon={<DeleteIcon/>}>delete</Button>
+    <Button variant="contained" color='warning' endIcon={<DeleteIcon/>}>delete</Button>
+    <br /><br />
+    <IconButton aria-label="delete" color='error'>
+        <DeleteIcon />
+      </IconButton>
+      <Button variant="contained" style={{backgroundColor:'grey'}} >contained</Button>
+      <Button variant="contained" sx={{backgroundColor:'grey'}} >contained</Button>
+
+      <FormGroup>
+      <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+      <FormControlLabel control={<Checkbox />} label="label" />
+    </FormGroup>
+      <Checkbox defaultChecked color="secondary" />
+      <Checkbox icon={<FavoriteBorder/>} checkedIcon={<FavoriteBorder/>}/>
+      <Checkbox onChange={()=>{
+        console.log("change done ")}}/>
+        <Button loading variant="plain">
+          Plain
+        </Button>
+    </>
   );
 }
-
 export default App;
